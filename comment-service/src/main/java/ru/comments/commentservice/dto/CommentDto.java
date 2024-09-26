2c7d9235da5f8d64dcc04d97ec71980eff213780
Egor <alexanoffegor@yandex.ru>
2024-09-26 17:28:03 +0300

    add controller tests

--- comment-service/src/main/java/ru/comments/commentservice/dto/CommentDto.java
+++ comment-service/src/main/java/ru/comments/commentservice/dto/CommentDto.java
@@ -1,5 +1,6 @@
 package ru.comments.commentservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CommentDto {
 
+    @JsonProperty("user_id")
     private Integer userId;
 
+    @JsonProperty("news_id")
     private Integer newsId;
 
     private String description;
